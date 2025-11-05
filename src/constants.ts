5dfdac6aadf3df82f466fd9be09b9b24cc6106dd
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-11-06 00:37:14 +0300

    Add type annotation for EMPTY_DISK_SIZE_KB constant for improved type safety

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const CONFIG_DIR: string = `${Deno.env.get("HOME")}/.openbsd-up`;
 export const DB_PATH: string = `${CONFIG_DIR}/state.sqlite`;
 export const LOGS_DIR: string = `${CONFIG_DIR}/logs`;
-export const EMPTY_DISK_SIZE_KB = 100;
+export const EMPTY_DISK_SIZE_KB: number = 100;
